1068afcdb66a9976651fd3f4b9b0bce5bde9b0f0
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-28 15:30:00 +0300

    Add files via upload

---
+++ chapter_3/3-4. Guest List.py
@@ -0,0 +1,7 @@
+guest=["muhammad ali","mike tyson","mbk-naboore"]
+
+
+print(f"You Are invited to my house Mr.{guest[0].title()}","at 7:00 pm.")
+print(f"You Are invited to my house Mr.{guest[1].title()}","at 7:00 pm.")
+print(f"You Are invited to my house Mr.{guest[2].title()}","at 7:00 pm.")
+
