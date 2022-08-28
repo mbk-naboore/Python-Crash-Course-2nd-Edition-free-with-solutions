04c7c7ca5f9e1fbb043475a07edc0578ca90df85
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-28 15:29:37 +0300

    Add files via upload

---
+++ chapter_3/3-3. Your Own List.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+
+print(f"{motorcycles[0].title()} is not that fastest.")
+print(f"this {motorcycles[1].title()} is powerful.")
+print(f"I love {motorcycles[2].title()} motorcycles.")
+print(f"{motorcycles[3].title()} is not my favorite one.")
+
+
