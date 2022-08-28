2c288b756f1740ce20bef23c9a05113654d6953c
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-28 15:29:16 +0300

    Add files via upload

---
+++ chapter_3/3-2. Greetings.py
@@ -0,0 +1,7 @@
+names = ["jack", "mike tyson", "muhammad ali", "mbk-naboore"]
+
+print(f"How are you doing {names[0].title()}?")
+print(f"How are you doing {names[1].title()}?")
+print(f"How are you doing {names[2].title()}?")
+print(f"How are you doing {names[3].title()}?")
+
