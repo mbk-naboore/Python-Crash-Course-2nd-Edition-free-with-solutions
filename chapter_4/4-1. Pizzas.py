4e82fb48f698ef244a1f919209568e60ea486b44
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-30 11:13:28 +0300

    Add files via upload

---
+++ chapter_4/4-1. Pizzas.py
@@ -0,0 +1,7 @@
+fav_pizza = ['Neapolitan Pizza', 'Chicago Pizza', 'New York-Style Pizza']
+
+for pizza in fav_pizza:
+    print(f"I love the {pizza.title()}", "It is the best.")
+print(f"The best pizzas are the {fav_pizza[0]},{fav_pizza[1]},{fav_pizza[2]}.")
+print("This shows how much I love pizza.")
+
