f163aec4ec4a1378a5ca166824397c6be2b97893
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-30 11:16:48 +0300

    Add files via upload

---
+++ chapter_4/4-7. Threes.py
@@ -0,0 +1,10 @@
+# the beginner way
+three_multi_2 = list(range(3,31,3))
+for numb in three_multi_2:
+    print(f"{numb}")
+
+
+# the advanced way only one line ......
+three_multi_1 = [print(numb) for numb in range(3, 31, 3)]
+
+
