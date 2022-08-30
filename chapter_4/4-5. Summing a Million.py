2e270e4bf3a22772babeae08493eb8f9a3a70399
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-30 11:15:46 +0300

    Add files via upload

---
+++ chapter_4/4-5. Summing a Million.py
@@ -0,0 +1,6 @@
+# this will give you a list with the numbers from 1 to 1000000.
+number_list = list(range(1, 1000001))
+min_number = min(number_list)
+max_number = max(number_list)
+sum_numbers = sum(number_list)
+print(f"the min number:{min_number} , the max number:{max_number}, the sum of the numbers:{sum_numbers}")
