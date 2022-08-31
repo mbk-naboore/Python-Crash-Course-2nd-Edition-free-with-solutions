cd79a479370680322767062d0e6872de1deed827
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-08-31 12:17:15 +0300

    Add files via upload

---
+++ chapter_4/4-10. Slices.py
@@ -0,0 +1,5 @@
+
+number_list = list(range(1, 51))
+print(f"the first three numbers in my list are:{number_list[:3]}")
+print(f"the mid three numbers in my list are:{number_list[23:26]}")
+print(f"the last three numbers in my list are:{number_list[-3:]}")
