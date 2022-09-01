6679d1f62fc49e79d2b00283b6f6bb7729a85e68
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-01 05:03:17 +0300

    Add files via upload

---
+++ chapter_5/5-5. Alien Colors #3.py
@@ -0,0 +1,28 @@
+# the first if will run:
+alien_color = "green"
+if alien_color == "green":
+    print("you have just earned 5 points")
+elif alien_color == "yellow":
+    print("you have just earned 10 points")
+else:
+    print("you have just earned 15 points")
+
+# the second elif will run:
+alien_color = "yellow"
+if alien_color == "green":
+    print("you have just earned 5 points")
+elif alien_color == "yellow":
+    print("you have just earned 10 points")
+else:
+    print("you have just earned 15 points")
+
+# the third elif will run:
+alien_color = "red"
+if alien_color == "green":
+    print("you have just earned 5 points")
+elif alien_color == "yellow":
+    print("you have just earned 10 points")
+else:
+    print("you have just earned 15 points")
+
+
