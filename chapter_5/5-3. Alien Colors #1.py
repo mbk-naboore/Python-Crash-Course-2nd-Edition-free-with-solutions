726d740fd17dd042dbfb79f28f94721f50099446
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-01 05:02:35 +0300

    Add files via upload

---
+++ chapter_5/5-3. Alien Colors #1.py
@@ -0,0 +1,10 @@
+# this is a True statement:
+alien_color = "green"
+if alien_color == "green":
+    print("you have just earned 5 points")
+
+# this is a False statement:
+alien_color = "yellow"
+if alien_color == "red":
+    print("you have just earned 5 points")
+
