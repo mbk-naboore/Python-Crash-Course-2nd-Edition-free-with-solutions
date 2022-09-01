1fbf4d1a8dac5cfacec5780b9d12b4c0ee0999ea
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-01 05:02:55 +0300

    Add files via upload

---
+++ chapter_5/5-4. Alien Colors #2.py
@@ -0,0 +1,15 @@
+# the if block is the one will run:
+alien_color = "green"
+if alien_color == "green":
+    print("you have just earned 5 points")
+else:
+    print("you have just earned 10 points")
+
+
+# the else block is the one will run:
+alien_color = "red"
+if alien_color == "green":
+    print("you have just earned 5 points")
+else:
+    print("you have just earned 10 points")
+
