858b5728749524f99ba5bd80d961823ccf402e8f
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-01 05:01:59 +0300

    Add files via upload

---
+++ chapter_5/5-1. Conditional Tests.py
@@ -0,0 +1,42 @@
+x = 10
+
+print("I think that x=10")
+print("your prediction was", x == 10)
+print()
+
+print("I think that x=20")
+print("your prediction was", x == 20)
+print()
+
+print("I think that x is even")
+print("your prediction was", x % 2 == 0)
+print()
+
+
+print("I think that x is odd")
+print("your prediction was", x % 2 != 0)
+print()
+
+print("I think that x ends with 0")
+print("your prediction was", str(x).endswith("0"))
+print()
+
+print("I think that x ends with 5")
+print("your prediction was", str(x).endswith("5"))
+print()
+
+print("I think that x starts with 1")
+print("your prediction was", str(x).startswith("1"))
+print()
+
+print("I think that x starts with 3")
+print("your prediction was", str(x).startswith("3"))
+print()
+
+print("I think that x is divisible by 10")
+print("your prediction was", x % 10 == 0)
+print()
+
+print("I think that x is divisible by 3")
+print("your prediction was", x % 3 == 0)
+print()
