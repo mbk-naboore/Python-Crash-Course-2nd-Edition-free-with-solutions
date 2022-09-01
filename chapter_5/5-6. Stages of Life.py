e51655df5bccfed1fb8db5107cf8388045bbec54
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-01 05:03:44 +0300

    Add files via upload

---
+++ chapter_5/5-6. Stages of Life.py
@@ -0,0 +1,15 @@
+# I will but 70 as example you can change it and try all the statements:
+age = 70
+
+if age < 2:
+    print("you are a baby")
+elif age < 4:
+    print("you are a toddler")
+elif age < 13:
+    print("you are a kid")
+elif age < 20:
+    print("you are a teenager")
+elif age < 65:
+    print("you are an adult")
+elif age >= 65:
+    print("you are an elder")
