f6841ef5661671479b9a376b43bca4c0d6001c00
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-02 19:15:15 +0300

    Add files via upload

---
+++ chapter_6/6-2. Favorite Numbers.py
@@ -0,0 +1,8 @@
+favorite_number = {
+    'jen': '7',
+    'sarah': '3',
+    'edward': '5',
+    'phil': '1',
+}
+for names, numbers in favorite_number.items():
+    print(f"{names.title()} favorite number is {numbers}.")
