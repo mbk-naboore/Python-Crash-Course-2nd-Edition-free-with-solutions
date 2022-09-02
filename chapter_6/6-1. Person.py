fe31edc337fe5f8ce5ec988725fe0a05d9cfa57f
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-02 19:12:49 +0300

    Add files via upload

---
+++ chapter_6/6-1. Person.py
@@ -0,0 +1,8 @@
+friend_info = {
+    "first_name": "mbk",
+    "last_name": "naboore",
+    "age": "24",
+    "city": "NYC"
+}
+for keys, value in friend_info.items():
+    print(f"My friends {keys.title()} is {value.title()}.")
