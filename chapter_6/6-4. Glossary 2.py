cb1c6ff93087ba4ec59f610b8473795ad90d9dc9
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-02 19:17:26 +0300

    Add files via upload

---
+++ chapter_6/6-4. Glossary 2.py
@@ -0,0 +1,11 @@
+glossary = {
+    "print": "tell python to print a sentence or an operation ",
+    "input": "allow the user to make input to the programme",
+    "for": "a small loop through list,dictionary or sit ",
+    "while": "the main way for loop if the condition is correct it will always work and keep going ",
+    ".sorted()": "it is a method that will organise list,dictionary or sit in alphapitical order"
+}
+
+for key, value in glossary.items():
+    print(key.title(), "the meaning is ::", value)
+
