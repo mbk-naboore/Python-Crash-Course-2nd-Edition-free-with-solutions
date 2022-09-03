3ddd8fa0efcaa06d717274e47751908abdc5b718
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-03 10:33:38 +0300

    Add files via upload

---
+++ chapter_7/7-2. Restaurant Seating.py
@@ -0,0 +1,6 @@
+user_input = int(input("Can you please tell me how many people will be in the dinner group?\n>>> "))
+
+if user_input > 8:
+    print("Sorry, you will have to wait for a table.")
+else:
+    print("The table is ready.")
