14b4b010075d02cdf502c8538c4b1028ff2add09
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-03 10:33:57 +0300

    Add files via upload

---
+++ chapter_7/7-3. Multiples of Ten.py
@@ -0,0 +1,6 @@
+number = int(input("Please give us a number:\t"))
+
+if number % 10 == 0:
+    print(f"The number {number} is a multiple of 10.")
+else:
+    print("The number you have provided is not a multiple of 10.")
