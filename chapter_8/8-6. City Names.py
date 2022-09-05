48abffae078e83ffd01c0593aac72fe40cef7589
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-05 09:55:10 +0300

    Add files via upload

---
+++ chapter_8/8-6. City Names.py
@@ -0,0 +1,8 @@
+def city_country(city, country):
+
+    return f"{city.title()}, {country.title()}"
+
+
+print(city_country(city="jakarta", country="Indonesia"))
+print(city_country(city="LA", country="US"))
+print(city_country(city="london", country="UK"))
