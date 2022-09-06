7e008439b5a9f92d0e50aea60dcd22596d094ac9
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-06 11:34:20 +0300

    Add files via upload

---
+++ chapter_8/8-15. Printing Models_part2.py
@@ -0,0 +1,7 @@
+import printing_models_part1
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+printing_models_part1.print_models(unprinted_designs, completed_models)
+printing_models_part1.show_completed_models(completed_models)
