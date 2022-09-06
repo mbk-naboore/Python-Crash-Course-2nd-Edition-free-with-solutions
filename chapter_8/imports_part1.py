712ee87d4850873ca7d08b9f5508fbb360c4698c
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-06 11:34:48 +0300

    Add files via upload

---
+++ chapter_8/imports_part1.py
@@ -0,0 +1,9 @@
+
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print(f"Making a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+
+
