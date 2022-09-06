c039502154902723cdf761110deebf8a4722226e
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-06 11:31:02 +0300

    Add files via upload

---
+++ chapter_8/8-9. Messages.py
@@ -0,0 +1,7 @@
+def printing_message(the_list):
+    for messages in the_list:
+        print(messages)
+
+
+list_message = ["Hello", "How are you doing", "I love you", "Have a nice day"]
+printing_message(list_message)
