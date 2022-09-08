f0b85f7f172069b9b9418f66374b997ec2f6178c
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-08 17:20:25 +0300

    Update 9-9. Battery Upgrade.py

--- chapter_9/9-9. Battery Upgrade.py
+++ chapter_9/9-9. Battery Upgrade.py
@@ -1,46 +1,78 @@
-class Users:
+class Car:
+    """A simple attempt to represent a car."""
 
-    def __init__(self, first_name, last_name, age, nationality):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-        self.nationality = nationality
-        self.login_attempts = 0
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
 
-    def describe_user(self):
-        print(f"\nUsers full name is {self.first_name.title()} {self.last_name.title()}.")
-        print(f"Users age is {self.age}.")
-        print(f"Users nationality is {self.nationality}.\n")
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
 
-    def greeting_user(self):
-        print(f"Hello,welcome back {self.first_name.title()} {self.last_name.title()}.")
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
 
-    def increment_login_attempts(self):
-        self.login_attempts += self.login_attempts
-        return self.login_attempts
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
 
-    def reset_login_attempts(self):
-        self.login_attempts = 0
-        return self.login_attempts
-
-
-class Admin(Users):
-    """the privileges of the admin user"""
-    def __init__(self, first_name, last_name, age, nationality):
-        super().__init__(first_name, last_name, age, nationality)
-        self.privileges = Privileges()
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
 
 
-class Privileges:
-    def __init(self):
-        self.privileges = ["admin can add post", "admin can delete post", "admin can ban user"]
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
 
-    def show_privileges(self):
-        print(f"The privileges for this User are:")
-        for privilege in self.privileges:
-            print(f"-{privilege}")
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+        # you can see that the default is 75 when the user does not provide it manually...
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+    def get_range(self):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge.")
+
+    def upgrade_battery(self):
+        if self.battery_size != 100:
+            self.battery_size = 100
 
 
-admin_mbk_naboore = Admin("mbk", "naboore", 26, "US")
-admin_mbk_naboore.privileges.show_privileges()
 
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery.battery_size}-kWh battery.")
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+
+# this will use the default 75 value (before upgrade):
+my_tesla.battery.get_range()
+
+my_tesla.battery.upgrade_battery()
+
+# this will use the 100 value (after upgrade):
+my_tesla.battery.get_range()
