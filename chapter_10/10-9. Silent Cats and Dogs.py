a51c915b52c1836c7e9040bb05dea0b29f3c3d21
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-12 10:42:44 +0300

    Add files via upload

---
+++ chapter_10/10-9. Silent Cats and Dogs.py
@@ -0,0 +1,17 @@
+file1 = "cats_10_8.txt"
+file2 = "dogs_10_8.txt"
+
+
+def printing_the_contents(file_path):
+    try:
+        with open(file_path) as file:
+            print(f"\nThe file ({file_path}) content is: ")
+            print(file.read().strip().title())
+            print("---------------------------------------------------")
+    except FileNotFoundError:
+        pass
+
+
+printing_the_contents(file1)
+printing_the_contents(file2)
+
