3765e5c74566669f7736e92ec7b3407f94baa537
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-14 20:35:35 +0300

    Update 10-13. Verify User.py

--- chapter_10/10-13. Verify User.py
+++ chapter_10/10-13. Verify User.py
@@ -1,1 +1,40 @@
+import json
+
+file_name = "username.json"
+
+
+def get_new_username():
+    username = input("What is your name: ")
+    with open(file_name, 'w') as f:
+        json.dump(username, f)
+    return username
+
+
+def get_stored_username():
+    try:
+        with open(file_name) as f:
+            username = json.load(f)
+            return username
+    except FileNotFoundError:
+        return None
+
+
+def check_username(username):
+    if username:
+        flag = input(f"Is [{username}] your username [y/n]? ").lower().strip()
+        if flag == 'y':
+            print(f"Welcome back, {username}!")
+            return True
+
+
+def greet_user_check_first():
+    username = get_stored_username()
+    if check_username(username):
+        return True
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+
+greet_user_check_first()
 
