ce79cb408ff714d7a03e22a99aa7d33b1846ff9a
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-14 20:33:32 +0300

    Update 10-11. Favorite Number.py

--- chapter_10/10-11. Favorite Number.py
+++ chapter_10/10-11. Favorite Number.py
@@ -1,1 +1,23 @@
+import json
+
+
+def fav_number_input():
+    number = int(input("Please tell us your favorite number:\t"))
+    with open("fav_number.json", "w") as f:
+        json.dump(number, f)
+        pass
+
+
+def fav_number_is():
+    try:
+        with open("fav_number.json", "r") as f:
+            number = json.load(f)
+            print(f"I know what is your favorite number! It is ( {number} ).")
+    except FileNotFoundError:
+        pass
+
+
+fav_number_input()
+
+fav_number_is()
 
