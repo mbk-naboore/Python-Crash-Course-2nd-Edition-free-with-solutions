4719266517867640ed2ca6da9a10545a70acc722
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-15 19:48:24 +0300

    Add files via upload

---
+++ chapter_11/city_functions_11_2.py
@@ -0,0 +1,13 @@
+def city_country(city, country, population=None):
+
+    if population:
+        return f"{city.title()}, {country.title()} - population {population}."
+    else:
+        return f"{city.title()}, {country.title()}"
+
+
+
+
+
+
+
