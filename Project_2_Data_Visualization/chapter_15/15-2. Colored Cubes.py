8d4264c8a0e166a056f18c8586f5159e58e1190e
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-21 16:31:18 +0300

    Update 15-2. Colored Cubes.py

--- Project_2_Data_Visualization/chapter_15/15-2. Colored Cubes.py
+++ Project_2_Data_Visualization/chapter_15/15-2. Colored Cubes.py
@@ -1,1 +1,20 @@
+import matplotlib.pyplot as plt
+
+# making the x-y values
+x_values = list(range(0, 5001))
+y_values = [x**3 for x in x_values]
+
+# creating the plot and style
+plt.style.use(["fivethirtyeight", "bmh"])
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Purples, s=30)
+
+# title and labels
+ax.set_title("Cubes", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Value**3", fontsize=14)
+ax.tick_params(axis='both', labelsize=14)
+
+# showing the output
+plt.show()
 
