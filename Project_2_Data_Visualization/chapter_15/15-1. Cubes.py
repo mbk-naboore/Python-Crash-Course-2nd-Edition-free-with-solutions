aaad343164efb1317aa4b2a29a6e7c25ecf132ae
mbk-naboore <111985767+mbk-naboore@users.noreply.github.com>
2022-09-21 16:28:40 +0300

    Update 15-1. Cubes.py

--- Project_2_Data_Visualization/chapter_15/15-1. Cubes.py
+++ Project_2_Data_Visualization/chapter_15/15-1. Cubes.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+
+#making the x-y values
+x_values = list(range(0, 5001))
+y_values = [x**3 for x in x_values]
+
+#creating the plot and style
+plt.style.use(["fivethirtyeight", "bmh"])
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values, linewidth=2.5)
+
+# title and labels
+ax.set_title("Cubes", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Value*Value*Value(cube)", fontsize=14)
+ax.tick_params(axis='both', labelsize=14)
+
+# showing the output
+plt.show()
+
 
